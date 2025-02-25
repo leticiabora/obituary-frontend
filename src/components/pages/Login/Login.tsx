b9d519e96bf736d57809bd93e864205cc41c3db2
Leticia Bora <leticia.bora@capitara.ai>
2025-02-25 18:11:22 -0300

    add login logic

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -1,21 +1,41 @@
+'use client';
+
+import { FormEvent } from 'react';
 import styles from './styles.module.css';
+import { getLogin } from '@/services/login';
 
 const Login = () => {
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const formData = new FormData(event.currentTarget);
+      const email = formData.get('email') as string;
+      const password = formData.get('password') as string;
+
+      const loggedUser = await getLogin({ email, password });
+
+      return loggedUser;
+
+    } catch (error) {
+      console.log(error);
+    }
+    
+  }
+
   return (
     <div>
-      <form className={styles.container}>
-        <label>
-          Name
-        <input aria-label="email" />
-        </label>
-        <label>
-          Email
-        <input aria-label="email" />
-        </label>
-        <label>
-          Password
-        <input aria-label="email" />
-        </label>
+      <form className={styles.container} onSubmit={onSubmit}>
+          <label>
+            Email
+          <input type="email" name="email" required />
+          </label>
+          <label>
+            Password
+          <input type="password" name="password" required />
+          </label>
+        <button>Login</button>
       </form>
     </div>
   )
