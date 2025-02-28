b071e9e6e2385f0091fc87e77f37fcbbbd6768d8
Leticia Bora <leticia.bora@capitara.ai>
2025-02-28 11:08:10 -0300

    add types

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -4,18 +4,21 @@ import { getLogin } from '@/services/login';
 import { createSession } from '../lib/session';
 import { redirect } from 'next/navigation';
 
-export async function loginUser(formData: FormData) {
+export async function loginUser(formData: FormData): Promise<void>  {
   try {
     const email = formData.get('email') as string;
     const password = formData.get('password') as string;
 
     const loggedUser = await getLogin({ email, password });
 
+    if (!loggedUser.token) {
+      throw new Error('Missing token!');
+    }
+    
     await createSession(loggedUser.token);
-  } catch (error) {
-    console.log(error);
 
-    return error;
+  } catch (error) {
+    throw error;
   }
 
   redirect('/');
