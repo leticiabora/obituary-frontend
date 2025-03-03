d23d0f6a3495ac7bd85810fd683e04cd75aa0d24
Leticia Bora <leticia.bora@capitara.ai>
2025-03-03 12:13:32 -0300

    add css and change icon

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Memorial</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
