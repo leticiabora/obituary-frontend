8766dd30012d1e750e9cb131c1b68f894a965a59
Leticia Bora <leticia.bora@capitara.ai>
2025-03-06 18:49:01 -0300

    add create memory and ts

--- src/app/lib/definitions.ts
+++ src/app/lib/definitions.ts
@@ -1,15 +1,19 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
 export const LoginFormSchema = z.object({
-  email: z.string().email({ message: 'Please enter a valid email.' }).min(5, { message: 'Email must be at least 5 characters long.' }).trim(),
+  email: z
+    .string()
+    .email({ message: 'Please enter a valid email.' })
+    .min(5, { message: 'Email must be at least 5 characters long.' })
+    .trim(),
   password: z
     .string()
     .min(6, { message: 'Password must be at least 6 characters long' })
     .trim(),
-})
+});
 
 export type LoginSchemaType = z.infer<typeof LoginFormSchema>;
- 
+
 export type LoginSchemaErrorType = {
   errors?: {
     [key: string]: string[] | undefined;
@@ -17,28 +21,34 @@ export type LoginSchemaErrorType = {
     password?: string[];
     name?: string[];
     confirmPassword?: string[];
-  }
+  };
   message?: string;
-}
- 
-export const SignupFormSchema = z.object({
-  name: z
-    .string()
-    .min(2, { message: 'Name must be at least 2 characters long.' })
-    .trim(),
-  email: z.string().email({ message: 'Please enter a valid email.' }).min(5, { message: 'Email must be at least 5 characters long.' }).trim(),
-  password: z
-    .string()
-    .min(6, { message: 'Password must be at least 6 characters long' })
-    .trim(),
-  confirmPassword: z.string(),
-}).refine((data) => data.password === data.confirmPassword, {
-  message: 'Passwords don\'t match',
-  path: ['confirmPassword'],
-});
+};
+
+export const SignupFormSchema = z
+  .object({
+    name: z
+      .string()
+      .min(2, { message: 'Name must be at least 2 characters long.' })
+      .trim(),
+    email: z
+      .string()
+      .email({ message: 'Please enter a valid email.' })
+      .min(5, { message: 'Email must be at least 5 characters long.' })
+      .trim(),
+    password: z
+      .string()
+      .min(6, { message: 'Password must be at least 6 characters long' })
+      .trim(),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords don\'t match',
+    path: ['confirmPassword'],
+  });
 
 export type SignupSchemaType = z.infer<typeof SignupFormSchema>;
- 
+
 export type AuthSchemaErrorType = {
   errors?: {
     [key: string]: string[] | undefined;
@@ -46,6 +56,37 @@ export type AuthSchemaErrorType = {
     password?: string[];
     name?: string[];
     confirmPassword?: string[];
-  }
+  };
   message?: string;
-}
+};
+
+// const ACCEPTED_IMAGE_TYPES = [
+//   'image/jpeg',
+//   'image/jpg',
+//   'image/png',
+//   'image/webp',
+// ];
+
+export const CreateMemorySchema = z.object({
+  title: z
+    .string()
+    .min(2, { message: 'Name must be at least 2 characters long.' })
+    .trim(),
+  description: z
+    .string()
+    .min(5, { message: 'Description must be at least 5 characters long.' })
+    .trim(),
+    image: z.any().refine((file) => file && file.size >= 1, { message: 'Image is required.' })
+});
+
+export type CreateMemory = z.infer<typeof CreateMemorySchema>;
+
+export type MemorySchemaErrorType = {
+  errors?: {
+    [key: string]: string[] | undefined;
+    title?: string[];
+    description?: string[];
+    image?: string[];
+  };
+  message?: string;
+};
--- src/app/memory/create/page.tsx
+++ src/app/memory/create/page.tsx
@@ -1,35 +1,46 @@
 'use client';
 
-import { loginUser } from '@/app/actions/auth';
 import Form from 'next/form';
 import { useActionState } from 'react';
-import { AuthSchemaErrorType } from '@/app/lib/definitions';
+import { MemorySchemaErrorType } from '@/app/lib/definitions';
+import { createMemory } from '../actions/actions';
+import styles from './styles.module.css';
 
-const initialState: AuthSchemaErrorType = {
+const initialState: MemorySchemaErrorType = {
   errors: {},
+  message: '',
 };
 
 const Memory = () => {
   const [state, action, pending] = useActionState<
-    AuthSchemaErrorType,
+  MemorySchemaErrorType,
     FormData
-  >(loginUser, initialState);
+  >(createMemory, initialState);
 
   return (
-    <div >
+    <div className={styles.container}>
       {state?.message && <p>{state?.message}</p>}
-      <Form  action={action}>
+      <Form action={action}>
         <div>
-          <label>
-            Email
-            <input type="email" name="email" required />
+          <label htmlFor="title">
+            Title
+            <input type="text" name="title" />
           </label>
+          {state?.errors?.title && <p className={styles.error}>{state.errors.title}</p>}
         </div>
         <div>
-          <label>
-            Password
-            <input type="password" name="password" required />
+          <label htmlFor="image">
+            Picture
+            <input type="file" name="image" accept="image/*" />
           </label>
+          {state?.errors?.image && <p className={styles.error}>{state.errors.image}</p>}
+        </div>
+        <div>
+          <label htmlFor="description">
+            Description
+            <textarea rows={10} name="description" />
+          </label>
+          {state?.errors?.description && <p className={styles.error}>{state.errors.description}</p>}
         </div>
         <button disabled={pending} type="submit">
           Login
