4e62c345a899ef28564e2690f64b5351d6ab3b34
Leticia Bora <leticia.bora@capitara.ai>
2025-03-06 23:23:15 -0300

    update about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,45 @@
+import styles from '../page.module.css';
+
+const AboutPage = () => {
+  return (
+    <div className={styles.title_container}>
+    <h2>About us</h2>
+    <div>
+    <p>Hey! I'm Leticia!</p>
+    <p>I lost my brother on 28th of April, 2023 when he was 30 years old. He went to the beach and drowned.</p>
+    <p>Yes, that day I felt that I lost all my strenght, I lost the ground, I lost my soul.</p>
+    <p>Exactly 1 year before (28th April, 2023) we were having one of the best days in our lifes: the Kiss concert.</p>
+    <p> 1 year later, I was living the worst day of my life.</p>
+    <p>On 20th of April 2023, we were having a best time in Hellowen's concert, where I sang with my entire heart: I'll have the best time of my life! Yesterday is history, tomorrow is a mystery.</p>
+    <p>Then, 1 week later... I lost my brother.</p>
+    <p>I could say that life could be tricky... but to be fair, it sucks sometimes. And sometimes it sucks consecutively.</p>
+
+    <p>What helped me deal with the grief?</p>
+
+    <p>We are a family that makes jokes about everything — my brother was probably the funniest one. He always made the best jokes and it included death and funerals, saying things like, "You can bury me in the backyard, but good luck digging the hole!" We always tried to make life lighter. So, when he passed away, these things helped me a lot during my grief. Every time I started to feel depressed, I remembered him talking about these things, making jokes, and it always helped me.</p>
+
+    <p>We bought a nice coffin, we bought flowers and when we were paying for all of this, I could hear his voice saying: 'Why spend money on that, dude?'</p>
+
+    <p>But, for the first time, we were able to command him :D</p>
+
+    <p>I knew the things that he probably didn't want and I started talking more about my own funeral, the things I want when my time comes, the things that I want to have there, things that I don't and people always feel uncomfortable. They say, "Stop talking about this!" But hey, that day will come for all of us. Talking about it won’t make it any less painful, but we can make it lighter—helping people remember us with good feelings. </p>
+    
+    <p>His death is still incredibly painful and try to make this lighter doesn't mean that you are feeling less. It will always hurt. But life keeps happening, and we have two choices: we can let ourselves drown in grief (which our loved ones wouldn’t want, right?), or we can keep living - even if with a giant wound in our hearts.</p>
+
+    <p>We do not pretend to say how you live your grief, we just want to help you to be able to try to deal with this pain or help your family to know how you wish them to react when you no longer be part of this world, knowing that you are not alone and your pain still is UNIQUE.</p>
+
+    <p>I was in pain, feeling like life was unfair, angry… (sometimes I still curse...) and then my mom said something tough to me on the day my brother was being buried (in the cemetery, not the backyard). She said: "We are not better than anyone to be spared from this."</p>
+
+    <p>The most harsh, painful truth — but she was right.</p>
+
+    <p>That’s why I started to think in create Grove Roots. To be able to create memorials for people (and pets!) that we love, without judgment and to be able to share our stories with our loved ones and our desires when our time comes.</p>
+
+    <p>Life cycle can be unfair and painful, you can curse, share your emotions, but at the same time, remember that from death, we can have life. We can learn, grow, and love. When a tree dies, it nourishes the soil so that other plants can grow. While we lose our loved ones, new lives are being born. One day, we celebrate the gift of life; the next, we feel the pain of its cycle.</p>
+
+    <p>If you're going through grief today, you are not alone. Sending you a virtual hug, and I can assure you—life deserves to be lived. And I bet your loved one agrees with me.</p>
+    </div>
+  </div>
+  );
+}
+
+export default AboutPage;
