a6147243e5f7c0ebbadb5cc31bba8cd9cbc20e01
Leticia Bora <leticia.bora@gmail.com>
2025-03-11 12:25:19 -0300

    start adding style on first page

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,6 @@
 export { default as PlantIcon } from './Plant';
 export { default as UserIcon } from './User';
-export { default as Logout } from './Logout';
+export { default as LogoutIcon } from './Logout';
+export { default as CurveArrowIcon } from './CurveArrow';
+export { default as SeedIcon } from './Seed';
+export { default as TreeIcon } from './Tree';
