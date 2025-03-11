b80eb1e5c7b85c0296ab5a55a6bdfc4275f8b471
Leticia Bora <leticia.bora@gmail.com>
2025-03-11 20:04:59 -0300

    add style on memory list

--- src/app/memorial/page.tsx
+++ src/app/memorial/page.tsx
@@ -1,6 +1,7 @@
 import { getMemories } from '@/services/memory';
 import Image from 'next/image';
 import Link from 'next/link';
+import styles from './styles.module.css';
 
 const Memories = async () => {
   const memories = await getMemories();
@@ -9,16 +10,13 @@ const Memories = async () => {
     return <div>No memories yet!</div>
   }
 
-  return <div>
+  return <div className={styles.container}>
     {memories.posts.map((memory) => {
       return (
-      <div key={memory.id}>
-        <p>{memory.id}</p>
-        {memory?.image && memory.image.length && <Image src={memory.image} width={200} height={200} alt={memory.title} />}
-        <p>{memory.title}</p>
-        <Link href={`/memorial/${memory.id}`}>Check Memory</Link>
-        {/* <p>{memory.description}</p> */}
-        <p>Created By: {memory?.author?.name}</p>
+      <div className={styles.memory} key={memory.id}>
+        {memory?.image && memory.image.length && <Image className={styles.image} src={memory.image} width={200} height={200} alt={memory.title} />}
+        <h3>{memory.title}</h3>
+        <Link className={styles.link} href={`/memorial/${memory.id}`}>Check Memory</Link>
         </div>
     )})}
   </div>
