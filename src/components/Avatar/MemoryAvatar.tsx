a847d84b8334c631f33828be4c5eb8c702d118c4
Leticia Bora <leticia.bora@gmail.com>
2025-03-14 19:29:48 -0300

    add memory type

--- src/components/Avatar/MemoryAvatar.tsx
+++ src/components/Avatar/MemoryAvatar.tsx
@@ -1,7 +1,12 @@
+import { Post } from '@/types/memory';
 import styles from './styles.module.css';
 import Image from 'next/image';
 
-const MemoryAvatar = ({ memory }) => {
+interface Memory {
+  memory: Post;
+}
+
+const MemoryAvatar = ({ memory }: Memory) => {
   return (
     <div className={styles.memory} key={memory.id}>
       {memory?.image && memory.image.length && (
